3d7d7c0503048ce114e5bffa890f58d1f70a4fd0
cbertola <cyrille.bertola@gmail.com>
2021-11-22 11:35:20 +0100

    new effects images

--- components/home/presentation.js
+++ components/home/presentation.js
@@ -1,16 +1,53 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-
+import { useState, useEffect } from 'react'
 export default function Presentation({ datas }) {
+	let X_MAX = -60
+	let X_MIN = -40
+	let Y_MAX = -60
+	let Y_MIN = -40
+	let [positionX, setPositionX] = useState((X_MAX + X_MIN) / 2);
+	let [positionY, setPositionY] = useState((Y_MAX + Y_MIN) / 2);
+	const mouveImg = (e) => {
+		
+		let d = document.querySelector('.profilPicture')
+
+		if (e.pageX < ( d.offsetLeft + (d.offsetWidth / 2)) && positionX < X_MIN  )
+			setPositionX(positionX => positionX + 1) 
+		else if (e.pageX > ( d.offsetLeft + (d.offsetWidth / 2)) && positionX > X_MAX )
+			setPositionX(positionX => positionX - 1)
+
+		if (e.pageY < ( d.offsetTop + (d.offsetHeight/ 2)) && positionY < Y_MIN )
+			setPositionY(positionY => positionY + 1)
+		else if (e.pageY > ( d.offsetTop + (d.offsetHeight/ 2)) && positionY > Y_MAX ){
+			setPositionY(positionY => positionY - 1)
+		}
+		console.log(positionY)
+		console.log(positionX)
+	}
+
+	useEffect(() => {
+        document.body.addEventListener('mousemove', mouveImg)
+		return () => {
+			document.body.removeEventListener('mousemove', mouveImg)
+		}
+
+    },[positionX, positionY]);
+	
+	
+
 	return (
 		<div className='presentation'>
 			<div className='profilPicture'>
 				<Image
 					priority
-					src="/images/profile2 copie.jpg"
-					layout="fill"
+					src="/images/profile.png"
+					layout="fixed"
+					height={600}
+					width={400}
 					alt='profile'
+					objectPosition={`${positionX}px ${positionY}px`}
 					quality={100}
 				/>
 			</div>
